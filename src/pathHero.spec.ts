b121882e1f345fc318cf9f5c03dea9786da002ea
Gilbrantas, Arijus <arijus.gilbrantas@aegon.nl>
2018-03-09 22:07:59 +0100

    add mocha

---
+++ src/pathHero.spec.ts
@@ -0,0 +1,14 @@
+import { assert } from "chai";
+
+describe("Path Hero", () => {
+
+    before(async () => {
+    });
+
+    after(async () => {
+    });
+
+    it("should be awesome", async () => {
+        assert.equal("awesome", "awesome");
+    });
+});
